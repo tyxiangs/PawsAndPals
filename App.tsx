8dc73373f52bf00d8ea47939e4797da792eb3793
gwen <e1157361@u.nus.edu>
2024-06-25 17:39:26 +0800

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,18 +1,16 @@
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { enableScreens } from 'react-native-screens';
 enableScreens();
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './app/screens/Login';
 import List from './app/screens/List';
 import Details from './app/screens/Details';
-import React, { useState, useEffect } from 'react';
 import { User, onAuthStateChanged } from 'firebase/auth';
 import { FIREBASE_AUTH } from './FirebaseConfig';
 
-
 const Stack = createNativeStackNavigator();
-
 const InsideStack = createNativeStackNavigator();
 
 function InsideLayout() {
@@ -28,22 +26,22 @@ export default function App() {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+    const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (user) => {
       console.log('user', user);
       setUser(user);
     });
-  }, [])
+    return unsubscribe; // Cleanup subscription on unmount
+  }, []);
 
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         {user ? (
-          <Stack.Screen name="Inside" component={InsideLayout} options={{headerShown: false}} />
+          <Stack.Screen name="Inside" component={InsideLayout} options={{ headerShown: false }} />
         ) : (
-          <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
-
+          <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         )}
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
