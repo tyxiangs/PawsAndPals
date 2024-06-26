6f38b1aded37f6334191e6c903d615f994b78dc0
khaixuen <163702646+khaixuen@users.noreply.github.com>
2024-06-26 17:43:19 +0800

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
@@ -6,12 +6,3 @@ const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.assetExts.push('cjs');
 
 module.exports = defaultConfig;
-=======
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
-
-const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.assetExts.push('cjs');
-
-module.exports = defaultConfig;
->>>>>>> dd0334bb80b9c33024309488173ac3fcabd153a5
